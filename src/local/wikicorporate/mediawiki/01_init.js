19524b9af210212eeb4ef6d1d64183042622c36d
wikicorporate <hazbin.hotel.wiki@gmail.com>
2026-09-02 01:28:45 +0300

    Delete src/local/wikicorporate/mediawiki/01_init.js

--- src/local/wikicorporate/mediawiki/01_init.js
+++
@@ -1,5 +0,0 @@
-/* CrossWikiActivity - Глобальный трекер активности */
-(async () => {
-    'use strict';
-    const localRequire = await mw.loader.using(["mediawiki.api", "mediawiki.ForeignApi", "mediawiki.util", "vue"]);
-    const Vue = localRequire("vue");
